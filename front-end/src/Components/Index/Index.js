58dd1b5e0bb2255e1697434a0791aad47a8117d9
jRodriguezIV <j.a.rodrigueziv@gmail.com>
2023-05-25 23:04:20 -0400

    updated Navbar functionality and design

--- front-end/src/Components/Index/Index.js
+++ front-end/src/Components/Index/Index.js
@@ -1,8 +1,34 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
+const API = process.env.REACT_APP_API_URL
+
 export default function Index() {
+    const [transactions, setTransactions] = useState([])
+
+    
+    useEffect(() => {
+        axios
+        .get(`${API}/transactions`)
+        .then(response => setTransactions(response.data))
+        .catch(e => console.warn("catch", e))
+    }, [])
+   
+    
+
+
+    
+
+
+
 
     return (
-        <div>
-            Index
+        <div className='index'>
+
+            <h1>Bank Account Total: {transactions.reduce((acc, cur) => acc + cur.amount, 0)}</h1>
+            
+
+
         </div>
     )
 }
--- front-end/src/Components/NavBar/NavBar.js
+++ front-end/src/Components/NavBar/NavBar.js
@@ -1,9 +1,17 @@
-export default function NavBar() {
+import { useNavigate } from "react-router-dom"
+import './NavBar.css'
 
+export default function NavBar() {
+    const navigate = useNavigate()
 
     return (
-        <div>
-            NavBar
-        </div>
+        <div className="navBar">
+            <div className="navBarText">
+
+                <p className="navIcon" onClick={() => navigate("/")}>Budget App</p>
+
+                <p className="newTransaction" onClick={() => navigate("/new")}>New Transaction</p>
+            </div>
+        </div>  
     )
 }
